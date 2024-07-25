5a2c16b5c04484fb581b5609f48cb26052630887
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-25 16:30:03 +0600

    get all products

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.post('/create-product', productController.createProduct);
 
+router.get('/', productController.getAllProducts);
+
+router.get('/:id', productController.getSingleProduct);
+
 export const productRoutes = router;
