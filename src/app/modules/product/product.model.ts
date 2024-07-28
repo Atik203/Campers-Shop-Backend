aee3fdc31373cff103332537a74282faefb9daab
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-29 00:00:05 +0600

    updated pre middlewares

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -29,7 +29,7 @@ export const productSchema = new Schema<TProduct>(
     stock: { type: Number, required: true, default: 0 },
     averageRating: { type: Number, default: 0 },
     brand: { type: String, required: true },
-    reviews: [reviewSchema],
+    reviews: reviewSchema,
     inStock: { type: Boolean, default: true },
     sizes: { type: [String], enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL'] },
     colors: [
@@ -45,41 +45,33 @@ export const productSchema = new Schema<TProduct>(
 );
 
 productSchema.pre('save', function (next) {
-  if (this.reviews && this.reviews.length > 0) {
-    const totalRating = this.reviews.reduce((sum, review) => {
-      const featuredRatings = review.featured.reduce(
-        (acc, authorReview) => acc + authorReview.rating,
-        0,
-      );
-      return sum + featuredRatings;
-    }, 0);
-
-    const totalReviews = this.reviews.reduce(
-      (sum, review) => sum + review.featured.length,
+  if (this.reviews && this.reviews.featured.length > 0) {
+    const totalRating = this.reviews.featured.reduce(
+      (sum, authorReview) => sum + authorReview.rating,
       0,
     );
+
+    const totalReviews = this.reviews.featured.length;
 
     this.averageRating = parseFloat((totalRating / totalReviews).toFixed(1));
 
     // Update totalCounts and counts for each rating section
-    this.reviews.forEach((review) => {
-      review.totalCounts = review.featured.length;
-      const ratingCounts = review.featured.reduce(
-        (acc: { [key: number]: number }, authorReview) => {
-          acc[authorReview.rating] = (acc[authorReview.rating] || 0) + 1;
-          return acc;
-        },
-        {} as { [key: number]: number },
-      );
+    this.reviews.totalCounts = totalReviews;
+    const ratingCounts = this.reviews.featured.reduce(
+      (acc: { [key: number]: number }, authorReview) => {
+        acc[authorReview.rating] = (acc[authorReview.rating] || 0) + 1;
+        return acc;
+      },
+      {} as { [key: number]: number },
+    );
 
-      review.counts = [
-        { rating: 1, count: ratingCounts[1] || 0 },
-        { rating: 2, count: ratingCounts[2] || 0 },
-        { rating: 3, count: ratingCounts[3] || 0 },
-        { rating: 4, count: ratingCounts[4] || 0 },
-        { rating: 5, count: ratingCounts[5] || 0 },
-      ];
-    });
+    this.reviews.counts = [
+      { rating: 1, count: ratingCounts[1] || 0 },
+      { rating: 2, count: ratingCounts[2] || 0 },
+      { rating: 3, count: ratingCounts[3] || 0 },
+      { rating: 4, count: ratingCounts[4] || 0 },
+      { rating: 5, count: ratingCounts[5] || 0 },
+    ];
   } else {
     this.averageRating = 0;
   }
