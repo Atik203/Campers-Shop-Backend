ed929b6f3fd59a3eed55e32f5f5bb6708caaf370
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-29 19:41:19 +0600

    added product filtering

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 
 type TResponse<T> = {
+  totalData?: number;
   success: boolean;
   message?: string;
   statusCode: number;
@@ -11,6 +12,7 @@ export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   res.status(data?.statusCode).json({
     success: data.success,
     message: data.message,
+    totalData: data.totalData,
     data: data.data,
   });
 };
