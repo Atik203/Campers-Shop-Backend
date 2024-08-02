d57d369afd5195c9c6b8fc32eedca8b1223cffc7
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-02 19:35:00 +0600

    update build

--- dist/app.js
+++ dist/app.js
@@ -22,7 +22,10 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:5173'],
+    origin: [
+        'https://campers-shop-frontend-lac.vercel.app',
+        'http://localhost:5173',
+    ],
     credentials: true, // allow cookies from the client
 }));
 // Routes
